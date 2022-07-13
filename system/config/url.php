e21d144824546b1504c895edaeeed3c5546c1095
IsaevEA <mikrit@gmail.com>
2022-07-14 00:49:32 +0300

    dev price

--- system/config/url.php
+++ system/config/url.php
@@ -14,6 +14,7 @@ return [
 		// These patterns should always fully match,
 		// as they are prepended with `^` and appended with `$`
 		'codmedbio',
+		'codmedbio.ru',
 		'codmedbio.ai-tech.ru'
 	],
 
